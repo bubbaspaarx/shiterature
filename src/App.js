fac0d6a0ea82c524517902f12c2622645df19d6a
Edward Sparks <edd_sparks@hotmail.com>
2019-02-14 11:08:51 +0000

    not a lot

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Router } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import logo from './rainbow-poo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+
+      </Router>
     );
   }
 }
